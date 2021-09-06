53c661b251ce80e2415249086dc831b0a6db82f9
Mohit Arora <mohit.arora@sydney.edu.au>
2021-09-06 13:53:43 +1000

    fixing helper.js code

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,9 +1,5 @@
 // get current date (alternate method date "return date.toLocaleDateString();")
 module.exports = {
-    // format_date: (date) => {
-      // Format date as MM/DD/YYYY
-      // return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
-      // return date.toLocaleDateString();
       format_date: (date) => {
         return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${
           new Date(date).getFullYear()
